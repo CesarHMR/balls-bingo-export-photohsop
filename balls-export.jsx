401fc8f45c0dd41947134db0739b8bb1163c4f93
Cesar Henrique Martins da Rosa <cesar_20002911@hotmail.com>
2022-03-13 16:50:37 -0300

    progress bar and improvements
    - alert message is better presented
    - alignment fix
    - progress bar implemented

--- balls-export.jsx
+++ balls-export.jsx
@@ -1,18 +1,19 @@
 (function CreateWindow(){
 
+    var windowTitle = "Bingo Balls Export"
+    var selectedFolder;
     var group;
     var panel;
     var window;
-    var selectedFolder;
 
-    window = new Window("dialog", "Balls Export");
+    window = new Window("dialog", windowTitle);
     panel = window.add("panel");
     group = panel.add("group");
     var btnFolderOutput = group.add("button", undefined, "Folder...")
     var txtFolderOutput = group.add("statictext", undefined, "", { truncate: "middle" });
     txtFolderOutput.preferredSize = [200,-1];
     group = window.add("group");
-    group.alignChildren = "center";
+    group.alignment = "center";
     var btnOk = group.add("button", undefined, "Ok");
     var btnCancel = group.add("button", undefined, "Cancel");
 
@@ -34,7 +35,7 @@
             Process(selectedFolder);
         }
         else{
-            alert("Select a folder to export the files!")
+            alert("Select a folder to export the files!", windowTitle, true);
         }
     }
 
@@ -45,7 +46,10 @@ function Process(selectedFolder){
     var doc = app.activeDocument;
     var ballsGroup = doc.layerSets.getByName('Balls');
     var titleElement = doc.layers.getByName('Text');
-    
+    var numberAmount = 2;
+    Progress("Reading folder...");
+    Progress.Set(numberAmount * ballsGroup.layers.length);
+
     //Set up
     ballsGroup.visible = true;
     titleElement.visible = true;
@@ -61,15 +65,47 @@ function Process(selectedFolder){
         
         ballsGroup.layers[ball].visible = true;
         
-        for (var number = 0; number < 2; number++) {
+        for (var number = 0; number < numberAmount; number++) {
 
             titleElement.textItem.contents = num;
+            Progress.Message(selectedFolder + "/ball-" + num + ".png");
             ExportPNG(selectedFolder.fullName, "ball-" + num);
             num++;
+            Progress.Increment();
         }
         
         ballsGroup.layers[ball].visible = false;
 
+    }
+
+    Progress.Close();
+
+    function Progress(initialMessage){
+    
+        var window = new Window("palette", "Exporting Files", undefined, { closeButton: false });
+        var text = window.add("statictext", undefined, initialMessage);
+        text.preferredSize = [450,-1];
+        var bar = window.add("progressbar", undefined, "");
+        bar.preferredSize = [450,-1];
+
+        Progress.Close = function(){
+            window.close();
+        }
+        Progress.Increment = function(){
+            bar.value ++;
+            window.update();
+        }
+        Progress.Message = function(message){
+            text.text = message
+        }
+        Progress.Set = function(steps){
+            bar.value = 0;
+            bar.minvalue = 0;
+            bar.maxvalue = steps;
+            window.update();
+        }
+        window.show();
+        window.update();
     }
 }
 
